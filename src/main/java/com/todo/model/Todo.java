d8173796a1900bd76e3ae87cb748c7c54490a1a4
Viktoriia Abakumova <abakumovaviktory@gmail.com>
2020-10-27 23:25:53 +0200

    TA-12 Improvements

--- src/main/java/com/todo/model/Todo.java
+++ src/main/java/com/todo/model/Todo.java
@@ -9,6 +9,8 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 import java.util.Date;
@@ -23,6 +25,7 @@ public class Todo {
     }
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private String id;
 
     @Indexed
--- src/test/java/com/todo/tests/model/TodoTest.java
+++ src/test/java/com/todo/tests/model/TodoTest.java
@@ -17,6 +17,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import java.util.Date;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -55,6 +57,8 @@ public class TodoTest {
     void verifyPostTodo() throws Exception {
         //given
         todoDto.setTitle("Test");
+        todoDto.setCreatedAt(new Date(System.currentTimeMillis()));
+        todoDto.setIsCompleted(false);
 
         //when
         mockMvc.perform(
